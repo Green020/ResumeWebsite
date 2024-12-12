d034b48d9f3dc656c52f04dc04d3f4e18ad3a50b
Dakota S <95447132+dSheetz020@users.noreply.github.com>
2024-12-11 22:16:30 -0500

    Logic Gates Coded
    Rework to function for the ladder logic items but base logic gates set

--- HTML Pages/LogicGates/logicgates.js
+++ HTML Pages/LogicGates/logicgates.js
@@ -1,16 +1,35 @@
 function RunLogicGate(){
-    var INPUT_1 = document.getElementById('INPUT_1').value;
+    var INPUT_1 = document.getElementById('INPUT_1').checked;
+    var INPUT_2 = document.getElementById('INPUT_2').checked;
     var GATE_1 = document.getElementById('GATE_1').value;
     var valResult;
 
     switch(GATE_1){
-        case "OPEN": break;
-        case "AND":  break;
-        case "OR" :  break;
-        case "XOR":  break;
-        case "NOT":  break;
-        case "NAND":  break;
-        case "NOR":  break;
-        case"XNOR":  break;
+        case "OPEN":
+            valResult = INPUT_1;
+            break;
+        case "AND":
+            valResult = INPUT_1 && INPUT_2; 
+            break;
+        case "OR":
+            valResult = INPUT_1 || INPUT_2; 
+            break;
+        case "XOR":
+            valResult = INPUT_1 !== INPUT_2; 
+            break;
+        case "NOT":
+            valResult = !INPUT_1; 
+            break;
+        case "NAND":
+            valResult = !(INPUT_1 && INPUT_2); 
+            break;
+        case "NOR":
+            valResult = !(INPUT_1 || INPUT_2); 
+            break;
+        case"XNOR":
+            valResult = (INPUT_1 && INPUT_2) || (!INPUT_1 && !INPUT_2);         
+            break;
     }
+
+    var GATE_1 = document.getElementById('OUTPUT_1').value = valResult;
 }
