50091b563749ae0950ef1d86e32a1fa00beb98ca
Dakota S <95447132+dSheetz020@users.noreply.github.com>
2024-12-11 21:01:17 -0500

    Updates
    Incomplete projects working on finding ladder logic example

---
+++ HTML Pages/LogicGates/logicgates.js
@@ -0,0 +1,16 @@
+function RunLogicGate(){
+    var INPUT_1 = document.getElementById('INPUT_1').value;
+    var GATE_1 = document.getElementById('GATE_1').value;
+    var valResult;
+
+    switch(GATE_1){
+        case "OPEN": break;
+        case "AND":  break;
+        case "OR" :  break;
+        case "XOR":  break;
+        case "NOT":  break;
+        case "NAND":  break;
+        case "NOR":  break;
+        case"XNOR":  break;
+    }
+}
