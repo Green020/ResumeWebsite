7c2e5a72eef41ccd216e9bc5fc23ff918b28b49d
Dakota S <95447132+dSheetz020@users.noreply.github.com>
2024-12-12 22:38:58 -0500

    Tic Tac Toe Functionality Added

--- scripting.js
+++ scripting.js
@@ -1,6 +1,105 @@
-/* ----- Variables & Arrays ----- */
+/* ----- Tic Tac Toe ----- */
+const statusDisplay = document.querySelector('.status');
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
+const winningMessage = () => {
+    return `${currentPlayer} has won!`;
+};
+const drawMessage = () => `TIE`;
+const TurnOrder = () => `${currentPlayer} turn`;
+
+statusDisplay.innerHTML = TurnOrder();
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', CellClicked));
+document.querySelector('.restartBTN').addEventListener('click', RestartGame);
+
+function CellClicked(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(
+        clickedCell.getAttribute('data-cell-index')
+    );
+
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+
+    CellPlayed(clickedCell, clickedCellIndex);
+    ResultValidation();
+}
+
+function CellPlayed(clickedCell, clickedCellIndex) {
+
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+function ResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break
+        }
+    }
+    if (roundWon) {
+        statusDisplay.innerHTML = winningMessage();
+        gameActive = false;
+        return;
+    }
+
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return;
+    }
+
+    PlayerChange();
+}
+
+function PlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = TurnOrder();
+}
+
+function RestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerHTML = TurnOrder();
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+}    
 
 /* ----- Functions and Events ----- */
+function menuToggle() {
+
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
 function RunLogicGate() {
     var INPUT_1 = document.getElementById('INPUT_1').checked;
     var INPUT_2 = document.getElementById('INPUT_2').checked;
@@ -13,7 +112,15 @@ function RunLogicGate() {
 
     switch (GATE_1) {
         case "OPEN":
-            valResult = INPUT_1;
+            if(INPUT_1 == true){
+                valResult = INPUT_1;
+            }
+            else if(INPUT_2 == true){
+                valResult = INPUT_2
+            }
+            else{
+                valResult = false;
+            }
             break;
         case "AND":
             valResult = INPUT_1 && INPUT_2;
@@ -36,6 +143,10 @@ function RunLogicGate() {
         case "XNOR":
             valResult = (INPUT_1 && INPUT_2) || (!INPUT_1 && !INPUT_2);
             break;
+        case "CLOSED":
+            valResult = (INPUT_1 && INPUT_2) || (!INPUT_1 && !INPUT_2);
+            break;
+            
     }
 
     if(valResult == true){
